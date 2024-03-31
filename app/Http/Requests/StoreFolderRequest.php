58a95d9d204a5cadca2dd0da17a2f36172062560
Salwan Al-Dulaimi <s.al-dulaimi@todayconcepts.com>
2024-03-31 12:07:42 +0200

    Create folder rules

--- app/Http/Requests/StoreFolderRequest.php
+++ app/Http/Requests/StoreFolderRequest.php
@@ -2,6 +2,11 @@
 
 namespace App\Http\Requests;
 
+use App\Models\File;
+use Illuminate\Validation\Rule;
+use Illuminate\Support\Facades\Auth;
+
+
 class StoreFolderRequest extends ParentIdBaseRequest
 {
     /**
@@ -19,8 +24,17 @@ class StoreFolderRequest extends ParentIdBaseRequest
      */
     public function rules(): array
     {
-        return [
-            'name' => ['required']
-        ];
+        return array_merge(
+            parent::rules(),
+            [
+                'name' => [
+                    'required',
+                    Rule::unique(File::class, 'name')
+                        ->where('create_at', Auth::id())
+                        ->where('parent_id', $this->parent_id)
+                        ->whereNull('deleted_at')
+                ]
+            ]
+        );
     }
 }
