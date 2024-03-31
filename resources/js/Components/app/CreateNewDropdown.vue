946f5d3fab9d8856135a5cde24da72e6ed168748
Salwan Al-Dulaimi <s.al-dulaimi@todayconcepts.com>
2024-03-31 12:06:22 +0200

    Style the Create Button

--- resources/js/Components/app/CreateNewDropdown.vue
+++ resources/js/Components/app/CreateNewDropdown.vue
@@ -1,5 +1,5 @@
 <template>
-    <Menu as="div" class="relative inline-block text-left">
+    <Menu as="div" class="relative block text-left">
         <div>
             <MenuButton
                 class="flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
