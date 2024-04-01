ecd9633799d93a9a0570d779775d54898a188753
Salwan Al-Dulaimi <s.al-dulaimi@todayconcepts.com>
2024-04-01 20:42:42 +0200

    Finished creating the folder :)

--- resources/js/Components/app/CreateFolderModal.vue
+++ resources/js/Components/app/CreateFolderModal.vue
@@ -1,25 +1,18 @@
 <template>
-    <modal :show="modelValue" max-width="sm">
+    <modal :show="modelValue" @show="onShow" max-width="sm">
         <div class="p-6">
             <h2 class="text-lg font-medium text-gray-900">Create New Folder</h2>
             <div class="mt-6">
-                <InputLabel
-                    for="forderName"
-                    value="Folder Name"
-                    class="sr-only"
-                />
-                <TextInput
-                    type="text"
-                    id="folderName"
-                    v-model="form.name"
-                    class="mt-1 black w-full"
-                    :class="
-                        form.errors.name
-                            ? 'border-red-500 focus:border-red-500 focus:ring-red-500'
-                            : ''
-                    "
-                    placeholder="Folder Name"
-                    @keyup.enter="createFolder"
+                
+                <InputLabel for="forderName" value="Folder Name" class="sr-only"/>
+                <TextInput  type="text" 
+                            ref="forlderNameInput"
+                            id="folderName"
+                            v-model="form.name"
+                            class="mt-1 black w-full" 
+                            :class="form.errors.name ? 'border-red-500 focus:border-red-500 focus:ring-red-500' : '' "
+                            placeholder="Folder Name"
+                            @keyup.enter="createFolder"
                 />
                 <InputError class="mt-2" :message="form.errors.name" />
             </div>
@@ -46,6 +39,7 @@ import SecondaryButton from "@/Components/SecondaryButton.vue";
 import InputError from "@/Components/InputError.vue";
 import { useForm } from "@inertiajs/vue3";
 import PrimaryButton from "../PrimaryButton.vue";
+import {nextTick, ref} from "vue";
 
 const form = useForm({
     name: "",
@@ -53,17 +47,33 @@ const form = useForm({
 
 const emit = defineEmits(["update:modelValue"]);
 
-function closeModal() {
-    emit("update:modelValue");
-    form.closeErrors();
-    form.reset();
+const forlderNameInput = ref(null)
+
+function onShow(){
+    nextTick(()=> forlderNameInput.value.focus() )
+    
+}
+
+const {modelValue} = defineProps({
+    modelValue: Boolean
+})
+
+function closeModal(){
+    emit ('update:modelValue')
+    form.clearErrors();
+    form.reset()
 }
 
 function createFolder() {
-    console.log("Create Folder");
+    form.post(route('folder.create'), {
+        preserveScroll: true,
+        onSuccess: () => {
+            closeModal()
+            form.reset()
+            forlderNameInput.value = ''
+        },
+        onError: () => forlderNameInput.value.focus()
+    })
 }
 
-const { modelValue } = defineProps({
-    modelValue: Boolean,
-});
 </script>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -9,7 +9,7 @@ import { Head } from "@inertiajs/vue3";
     <AuthenticatedLayout>
         <template #header>
             <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-                123Dashboard
+                Dashboard
             </h2>
         </template>
 
