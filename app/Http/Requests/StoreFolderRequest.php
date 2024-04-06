e85b03f8d57654c91c9ecdc63d41d1d83cabceac
Salwan Al-Dulaimi <s.al-dulaimi@todayconcepts.com>
2024-04-06 09:39:29 +0200

    Small bug fix

--- app/Http/Requests/StoreFolderRequest.php
+++ app/Http/Requests/StoreFolderRequest.php
@@ -30,7 +30,7 @@ class StoreFolderRequest extends ParentIdBaseRequest
                 'name' => [
                     'required',
                     Rule::unique(File::class, 'name')
-                        ->where('created_at', Auth::id())
+                        ->where('created_by', Auth::id())
                         ->where('parent_id', $this->parent_id)
                         ->whereNull('deleted_at')
                 ]
