a2f2a46517a6e2a64c5edcfa5ea65a4715715b60
Salwan Al-Dulaimi <salw-91@hotmail.com>
2024-04-12 22:14:32 +0200

    Update FileUploadMenuItem.vue
    Just a small change to check git account.

--- resources/js/Components/app/FileUploadMenuItem.vue
+++ resources/js/Components/app/FileUploadMenuItem.vue
@@ -1,5 +1,11 @@
-<MenuItem v-slot="{ active }">
-    <a href="" class="text-gray-700 block px-4 py-2 text-sm">Upload Folder</a>
-</MenuItem>
-
+<template>
+    <MenuItem v-slot="{ active }">
+    <a href="" class="text-gray-700 block px-4 py-2 text-sm">
+        Upload Folder
+        <input type="file" class='absolute left-0 top-0 bottom-0 right-0 cursor-pointer opacity-0'>
+    </a>
+    </MenuItem>
+</template>
+<script setup>
 import { MenuItem } from "@headlessui/vue";
+</script>
