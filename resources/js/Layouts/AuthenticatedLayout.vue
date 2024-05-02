89166cf49ccc66771319f60e31f4f96f27b4411e
Salwan Al-Dulaimi <salw-91@hotmail.com>
2024-05-02 08:52:27 +0200

    Work on drag and drop files

--- resources/js/Layouts/AuthenticatedLayout.vue
+++ resources/js/Layouts/AuthenticatedLayout.vue
@@ -2,15 +2,24 @@
     <div class="h-screen bg-gray-50 flex w-full gap-4">
         <Navigation />
 
-        <main @drop.prevent="handleDrop" @dragover.prevent="onDragOver" @dragleave.prevent="onDragLeave"
-            class="flex flex-col flex-1 px-4 overflow-hidden">
-            <div class="flex items-center justify-between w-full">
-                <SearchForm />
-                <UserSettingDropdown />
-            </div>
-            <div class="flex-1 flex flex-col overflow-hidden">
-                <slot />
-            </div>
+        <main @drop.prevent="handleDrop" 
+              @dragover.prevent="onDragOver" 
+              @dragleave.prevent="onDragLeave"
+            class="flex flex-col flex-1 px-4 overflow-hidden"
+            :class="dragOver ? 'dropzone' : ''">
+
+            <template v-if="dragOver" class="text-gray-500 text-center py-8 text-sm">
+                Drop files here to upload
+            </template>
+            <template v-else>
+                <div class="flex items-center justify-between w-full">
+                    <SearchForm />
+                    <UserSettingDropdown />
+                </div>
+                <div class="flex-1 flex flex-col overflow-hidden">
+                    <slot />
+                </div>
+            </template>
         </main>
     </div>
 </template>
@@ -20,7 +29,7 @@
 import Navigation from '@/Components/app/Navigation.vue';
 import SearchForm from '@/Components/app/SearchForm.vue';
 import UserSettingDropdown from '@/Components/app/UserSettingDropdown.vue';
-import { onMounted } from "vue";
+import { onMounted, ref } from "vue";
 import { emitter, FILE_UPLOAD_STARTED } from '@/event-bus.js';
 
 const dragOver = ref(false)
@@ -51,3 +60,16 @@ onMounted(() => {
 })
 
 </script>
+
+<style scoped>
+ .dropzone{
+    width: 100%;
+    height: 100%;
+    color: #8d8d8d;
+    border: 2px dashed gray;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+ }
+
+</style>
